213003bce4877e2e08736a0c9249cfee9a4b98e5
TheNootLinja <thenootlinja@gmail.com>
2026-01-31 15:05:08 -0500

    add to openNoteDialog function

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ function openNoteDialog(){
     const dialog = document.getElementById('noteDialog');
     const titleInput = document.getElementById('noteTitle');
     const contentInput = document.getElementById('noteContent');
+
+    dialog.showModal();
+    titleInput.focus();
 }
